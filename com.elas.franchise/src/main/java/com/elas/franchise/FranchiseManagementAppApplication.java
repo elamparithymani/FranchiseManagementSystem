7c67443319163e3bdcbbb75dd79316b5b32e6f24
elamp <elamp@DESKTOP-4K9VT94>
2023-07-02 18:41:55 +0530

    Initial commit

---
+++ com.elas.franchise/src/main/java/com/elas/franchise/FranchiseManagementAppApplication.java
@@ -0,0 +1,14 @@
+package com.elas.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseManagementAppApplication.class, args);
+	}
+
+}
+ 
---
+++ com.elas.franchise/src/test/java/com/elas/franchise/FranchiseManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elas.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
